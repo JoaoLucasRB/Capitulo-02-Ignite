aa4b98f3f17c1f260e52f0308a57bf202b39fd01
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2021-06-03 00:23:51 -0300

    Modulo-02

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,9 +4,10 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #e52e4d;
+    --green: #33cc95;
     --blue: #5429cc;
 
-    -blue-light: #6933ff;
+    --blue-light: #6933ff;
 
     --text-title: #363f5f;
     --text-body: #969cb3;
@@ -32,7 +33,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
